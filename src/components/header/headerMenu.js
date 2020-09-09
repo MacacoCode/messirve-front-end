23606fca5da0b78777e34d29b87d1817a7a9a838
Luz Clarita <luzpm@uamv.edu.ni>
2020-09-09 14:09:04 -0600

    lo de la imagen

--- src/components/header/headerMenu.js
+++ src/components/header/headerMenu.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { Menu } from 'antd';
+import logo from '../../img/logo.png'
 
 const { SubMenu } = Menu;
 
 const HeaderMenu = () => {
   return (
     <Menu mode="horizontal" theme="light">
-        <Menu.Item key="logo">
-        Logo
-        </Menu.Item>
-        <Menu.Item key="home">
+      <Menu.Item key="logo">
+        <img src={logo} alt="Logo" width="120" height="auto" />
+      </Menu.Item>
+      <Menu.Item key="home">
         Home
         </Menu.Item>
-        <SubMenu title="Fitness">
+      <SubMenu title="Fitness">
         <Menu.ItemGroup title="Vestimenta">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          <Menu.Item key="setting:1">Option 1</Menu.Item>
+          <Menu.Item key="setting:2">Option 2</Menu.Item>
         </Menu.ItemGroup>
         <Menu.ItemGroup title="Suplementos">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          <Menu.Item key="setting:3">Option 3</Menu.Item>
+          <Menu.Item key="setting:4">Option 4</Menu.Item>
         </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="logIn" style={{ float: 'right' }}>
+      </SubMenu>
+      <Menu.Item key="logIn" style={{ float: 'right' }}>
         Log In
         </Menu.Item>
-        <Menu.Item key="signIn" style={{ float: 'right' }}>
+      <Menu.Item key="signIn" style={{ float: 'right' }}>
         Sign In
         </Menu.Item>
     </Menu>
