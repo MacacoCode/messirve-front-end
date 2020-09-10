dfa0d73b925157c5ecaf277b2137e0f45160ec7b
Maruco420 <marcosnarvaez2010@gmail.com>
2020-09-09 23:24:20 -0600

    More styles

--- src/components/header/headerMenu.js
+++ src/components/header/headerMenu.js
@@ -8,13 +8,13 @@ const { SubMenu } = Menu;
 const HeaderMenu = () => {
   return (
     <Menu mode="horizontal" theme="light">
-      <Menu.Item key="logo">
+      <Menu.Item key="logo" style={{ float: 'left' }}>
         <img src={logo} alt="Logo" width="120" height="auto" />
       </Menu.Item>
-      <Menu.Item key="home">
+      <Menu.Item key="home" style={{ float: 'left' }}>
         Home
       </Menu.Item>
-      <SubMenu title="Fitness">
+      <SubMenu title="Fitness" style={{ float: 'left' }}>
         <Menu.ItemGroup title="Vestimenta">
           <Menu.Item key="setting:1">Option 1</Menu.Item>
           <Menu.Item key="setting:2">Option 2</Menu.Item>
@@ -24,10 +24,10 @@ const HeaderMenu = () => {
           <Menu.Item key="setting:4">Option 4</Menu.Item>
         </Menu.ItemGroup>
       </SubMenu>
-      <Menu.Item key="about">
+      <Menu.Item key="about" style={{ float: 'left' }}>
         About
       </Menu.Item>
-      <Menu.Item style={{ width: '50%' }}>
+      <Menu.Item style={{ width: '50%', marginTop: 10 }} >
         <SearchBar />
       </Menu.Item>
       <Menu.Item key="logIn" style={{ float: 'right' }}>
