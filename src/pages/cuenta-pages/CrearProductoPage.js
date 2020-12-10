863b5341afefe30e2b47bf67eb5d09e2f9a76fca
Maruco420 <marcosnarvaez2010@gmail.com>
2020-12-10 13:29:22 -0600

    Quitar inutilidad

--- src/pages/cuenta-pages/CrearProductoPage.js
+++ src/pages/cuenta-pages/CrearProductoPage.js
@@ -1,7 +1,4 @@
-import { message } from 'antd';
-import { isEmpty } from 'lodash';
-import React, { useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import React from 'react';
 import { connect } from 'unistore/react';
 import CrearProducto from '../../components/administrar-productos/CrearProducto';
 
